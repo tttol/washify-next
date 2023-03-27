b2fd6eb8de1ab3aa8897f0b288fcb958c48cbe90
t-takahashi <tkhstol.929@gmail.com>
2023-03-27 22:37:48 +0900

    dotenv削除、ビジネスロジック実装中

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import scanAll from '../lib/client/ddb/scan-all'
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import scanAll from "../lib/client/ddb/scan-all";
+import Card from "@/component/Card";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export async function getItems() {
-  const items = await scanAll();
-  return items;
+export async function getServerSideProps() {
+  const _items: any = await scanAll();
+  const items: string = JSON.stringify(_items);
+  console.debug(`items=${items}`);
+  // console.debug(`items[0]=${items[0]}`)
+  // console.debug(`items[1]=${items[1]}`)
+  // console.debug(`items[2]=${items[2]}`)
+  // console.debug(`items[3]=${items[3]}`)
+  return { props: { items } };
 }
 
-export default function Home() {
+export default function Home({ items }: { items: string }) {
   return (
     <>
       <Head>
@@ -20,26 +27,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="w-4/5 bg-[var(--card-bg-color)] mx-auto my-10 rounded-3xl p-7 relative">
-          <p className="text-white text-4xl font-semibold">CARD_TITLE</p>
-          <div className="flex mt-10">
-            <p className="text-[var(--text-date-color)] text-2xl">CARD_DATE</p>
-            <div className="flex ml-auto">
-              <div className="mr-5">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10 text-white">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                </svg>
-              </div>
-              <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10 text-white">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                </svg>              
-              </div>
-            </div>
-          </div>
-          <span className="BADGE_CLASS">!</span>
-        </div>
+        <Card item={items}></Card>
       </main>
     </>
-  )
+  );
 }
